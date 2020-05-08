4aa5a315a4934afa3ebf552d90930f8f7a4412ec
adriano <adriano20fatec@gmail.com>
2020-05-08 01:14:57 -0300

    subindo

--- viacep.php
+++ viacep.php
@@ -9,9 +9,8 @@ $address = (object)[
 ];
 if(isset($_POST['cep'])){
 $cep = $_POST['cep'];
-$cep = str_replace('_','',$cep);
-$cep = str_replace('+','',$cep);
-$cep = str_replace(' ','',$cep);
+$cep = preg_replace('/[^0-9]/','',$cep);
+
 $url = 'https://viacep.com.br/ws/'.$cep.'/json/';
 $address = json_decode(file_get_contents($url));
 
