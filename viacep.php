0022d52a1b4a329a26a5f3c18f5226ece4719562
adriano <adriano20fatec@gmail.com>
2020-05-13 21:32:07 -0300

    Refatoração 13.05.2020

--- viacep.php
+++ viacep.php
@@ -1,17 +1,41 @@
 <?php 
-$address = (object)[
-    'cep'=>'',
-    'logradouro'=>'',
-    'bairro'=>'',
-    'localidade'=>'',
-    'uf'=>''
 
-];
+
+function getAdrdress(){
+ 
 if(isset($_POST['cep'])){
 $cep = $_POST['cep'];
-$cep = preg_replace('/[^0-9]/','',$cep);
+$cep =filterCep($cep);
 
-$url = 'https://viacep.com.br/ws/'.$cep.'/json/';
-$address = json_decode(file_get_contents($url));
 
+$address = getAdrdressViacep($cep);
+
+}else{
+    $address = addressEmpy();
+}
+
+return $address;
+
+}
+function addressEmpy(){
+    return  (object)[
+        'cep'=>'',
+        'logradouro'=>'',
+        'bairro'=>'',
+        'localidade'=>'',
+        'uf'=>''
+    
+    ];
+}
+function filterCep(String $cep):String{
+return  preg_replace('/[^0-9]/','',$cep);
+}
+function isCep():bool{
+
+}
+function getAdrdressViacep(String $cep){
+
+
+    $url = 'https://viacep.com.br/ws/'.$cep.'/json/';
+    return json_decode(file_get_contents($url));
 }
