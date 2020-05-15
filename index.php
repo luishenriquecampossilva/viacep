5ca13beb9f3ec74195c3722d1285a4e7eace55a6
adriano <adriano20fatec@gmail.com>
2020-05-14 23:36:25 -0300

    Headers 14-05-2020

--- index.php
+++ index.php
@@ -1,12 +1,55 @@
 
-<?php
-include('headers.php');
 
-$address = getAdrdress();
+
+
+<?php
+  
+
+
+    $select = $_POST['cars']??0;
+
+    if($select == 'postmon'){
+
+        include('postmon.php');
+
+        $address = getAdrdress();
+
+    }
+
+    elseif($select == 'viacep'){
+
+        include('viacep.php');
+
+        $address = getAdrdress();
+
+    }
+
+
+    elseif($select == 'headers'){
+
+        include('headers.php');
+
+        $address = getAdrdress();
+
+    }
+    else{
+
+ include('viacep.php');
+
+        $address = getAdrdress();
+
+    }
+
+
+
+   
+    
+
+
+
+
 
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -17,12 +60,24 @@ $address = getAdrdress();
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="." method="post">
+
+    <form action="" method="post">
+
+
+    <select  name="cars">
+  <option value="viacep">selecione</option>
+  <option value="viacep">viacep</option>
+  <option value="postmon">postmon</option>
+  <option value="headers">headers</option>
+  
+</select>
+ 
+
         <p>Digite o CEP para encontrar o endereço.</p>
         <input type="text" placeholder="Digite um cep..." name="cep" value = "<?php
         echo $address->cep;
         ?>">
-        <input type="submit">
+        <input type="submit" id="cars">
         <input type="text" placeholder="rua" name="rua" value = "<?php
         echo $address->logradouro;
         ?>">
